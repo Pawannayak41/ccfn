339de7c41286416dd9f0c5038939913dbc9fbd8d
Pawannayak41 <88264105+Pawannayak41@users.noreply.github.com>
2025-03-17 22:21:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://backend13-b5c3a9eybbd7epg3.centralindia-01.azurewebsites.net/";
+const API_URL = "https://backend13-b5c3a9eybbd7epg3.centralindia-01.azurewebsites.net/products";
 
 const App = () => {
   const [products, setProducts] = useState([]);
